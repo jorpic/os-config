9f0cbd0756e111089b3e2bb52c615f95c3c75c65
Max Taldykin <jorpic@gmail.com>
2018-05-04 21:42:44 +0300

    Minimal usable configuration

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -11,35 +11,45 @@
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "zen";
+  networking.wireless = {
+    enable = true;
+    interfaces = [ "wlp2s0" ];
+    userControlled.enable = true;
+    # v--- this is required to autogenerate wpa_supplicant.conf
+    networks = { dummy = {}; };
+  };
+  networking.connman.enable = true;
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 
   # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Moscow";
 
   # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  # environment.systemPackages = with pkgs; [
-  #   wget vim
-  # ];
+  environment.systemPackages = with pkgs; [
+    wget vim git
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
+  programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   # List services that you want to enable:
+
+  services.ntp.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -48,7 +58,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -65,15 +75,12 @@
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  # };
+  users.mutableUsers = false;
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$1$bUCc9/PD$GcGnBauKqhCEvI8nrW4XP/";
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
